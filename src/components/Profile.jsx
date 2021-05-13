9af498837652d2f228e0d9742bdb5174a90420f2
Anastasya <AnastasyaShapovalova>
2021-05-13 23:49:54 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdVvEDkKjNI0i6ymK6zWKDCId_yfmnRV2Wafh6-_vQHJed1Zdopbj8YRzKnIRHjaUnseA&usqp=CAU"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
